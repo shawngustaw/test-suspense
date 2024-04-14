cdf3e22135a8f46e41070c90126e6b79715eeef8
shawngustaw <srgustaw@gmail.com>
2024-04-14 13:08:44 -0700

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function RootLoadingState() {
   return (
-    <div>
+    <div style={{ backgroundColor: 'blue'}}>
       <h1>ROOT Loading... you should only see this on the root route</h1>
     </div>
   );
--- src/app/test/[id]/loading.tsx
+++ src/app/test/[id]/loading.tsx
@@ -1,6 +1,6 @@
 export default function TestLoadingState() {
   return (
-    <div>
+    <div style={{ backgroundColor: 'green'}}>
       <h1 style={{ color: "red" }}>
         You only see this navigating to the dynamic route
       </h1>
